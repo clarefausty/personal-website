3e47fc91cc99bc4a49ae2fdd77a25542c72e54a9
clarefausty <clarefausty@gmail.com>
2023-07-25 16:56:06 +0100

    footer commit

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import "./Footer.css"
 
 function Footer() {
+  const [year, setYear] = React.useState(new Date().getFullYear())
+  React.useEffect(()=>{
+    const interval = setInterval(()=>{
+      setYear(new Date().getFullYear())
+    }, 1000)
+    return() => clearInterval(interval)
+  }, [])
   return (
-    <div>Footer</div>
+    <React.Fragment>
+    <div className='foot-end'>
+      <p>FaustyCodes @ {year}</p>
+    </div>
+    </React.Fragment>
+    
   )
 }
 
