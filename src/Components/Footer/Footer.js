473a9b3ae32f2cdc6fa4f3d8ff63f1423efac6a7
clarefausty <clarefausty@gmail.com>
2023-07-25 17:34:29 +0100

    footer development

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./Footer.css"
+import { Marquee } from 'dynamic-marquee-react'
 
 function Footer() {
   const [year, setYear] = React.useState(new Date().getFullYear())
@@ -12,7 +13,11 @@ function Footer() {
   return (
     <React.Fragment>
     <div className='foot-end'>
-      <p>FaustyCodes @ {year}</p>
+      <Marquee rate={-100}>
+      <div>FaustyCodes @ {year}</div>
+      <div>&nbsp;&bull;&nbsp;</div>
+      </Marquee>
+      
     </div>
     </React.Fragment>
     
